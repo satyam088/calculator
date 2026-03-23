264317270e07ab06238269a905b5af0921aed564
Satyam Chauhan <satyamchauhan9656@gmail.com>
2026-03-23 19:55:01 +0530

    I created a calculate fucntion and used math JS libarary to evaluate

--- newscript.js
+++ newscript.js
@@ -1,11 +1,27 @@
 const numBtn = document.querySelectorAll(".same");
-const result = document.querySelector("#res");
+const res = document.querySelector("#res");
 const clear = document.querySelector("#clear");
+const main = document.querySelector("#main");
 // console.log(numBtn);
 const numbers = ['0','1','2','3','4','5','6','7','8','9'];
 const operators = ['+','-','*','/'];
 function calculate(){
     let stringToBeclaculated = res.textContent;
+    try{
+        let ans = math.evaluate(res.textContent);
+        res.textContent = ans;
+    }catch(err){
+        res.textContent= 0;
+        const p = document.createElement('p');
+        p.textContent= "Expression is Invalid";
+        p.style.color = 'red';
+        p.style.paddingTop = '20px';
+        main.append(p);
+        let timeout = setTimeout(()=>{
+            p.remove();
+            clearTimeout(timeout);
+        },3000);
+    }
 }
 function addListners(){
     Array.from(numBtn).forEach((btn)=>{
@@ -35,12 +51,20 @@ function addListners(){
                 res.textContent = dets.key;
             else
                 res.textContent+= dets.key;
+        }else if(operators.includes(dets.key)){
+            if(res.textContent!=0){
+                res.textContent+=dets.key;
+            }
         }
         if(dets.key =='Backspace')
             if(res.textContent.length == 1)
                 res.textContent=0;
             else 
                 res.textContent = res.textContent.substring(0, res.textContent.length -1);
+        if(dets.key=='Enter'){
+            calculate();
+        }
+        
     });
 }
 addListners();
